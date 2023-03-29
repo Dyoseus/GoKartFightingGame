f9c4b900e9e58e1ec27fbd7a05b5835848bfbe3a
Albert Le <albert_h_le@hotmail.com>
2023-03-29 14:32:32 -0400

    Added Circle Collider for movement

--- Go Kart Fighting Game/Assets/Scripts/GoKartController.cs
+++ Go Kart Fighting Game/Assets/Scripts/GoKartController.cs
@@ -24,26 +24,7 @@ public class GoKartController : NetworkBehaviour
     void Update()
     {
         if (IsLocalPlayer){
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
-
-            Vector3 movement = transform.forward * vertical * speed * Time.deltaTime;
-            rb.MovePosition(rb.position + movement);
-            transform.Rotate(0, horizontal * rotationSpeed * Time.deltaTime, 0);
-
-            // Move the parent along with the child object
-            parentTransform.position = rb.position;
-
-            // Check for slope
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, slopeForceRayLength, groundLayer))
-            {
-                Vector3 slopeForceDirection = Vector3.Cross(hit.normal, transform.right).normalized;
-                rb.AddForce(slopeForceDirection * slopeForce, ForceMode.Acceleration);
-            }
-
-            // Apply gravity
-            rb.AddForce(Vector3.down * gravity);
+            
         }
     }
 
